c5f57c73b174b6a3e0aab112ed3c3b907a10b9e3
Dami Brown <damilola.brown3992@gmail.com>
2015-05-09 15:59:47 +0100

    Hello World

---
+++ hello.java
@@ -0,0 +1,11 @@
+class hello
+{
+	public static void main(String[] param){
+		helloMessage();
+		System.exit(0);
+	}
+	public static void helloMessage(){
+		System.out.println("Hello");
+return;
+	}
+}
