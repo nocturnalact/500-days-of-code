319fee81d264427688f99230eefdf9c01d60c560
Dami Brown <tinaecole@yahoo.com>
2015-06-03 23:16:10 +0100

    with comments

--- Guess.java
+++ Guess.java
@@ -12,12 +12,12 @@ class Guess{
 		System.exit(0);
 	}
 	public static void ask(){
-		String question;
-		String answer;
 		Random r = new Random();
+		int num;
+		int guesser;
 
-		int num = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1-10?"));
-		int guesser = r.nextInt(10 - 1) + 1;
+		num = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1-10?"));
+		guesser = r.nextInt(10 - 1) + 1; // sets the boundary between 1 and 10
 
 				
 		JOptionPane.showMessageDialog(null, "You guessed " + num + "\n I guessed " + guesser);
