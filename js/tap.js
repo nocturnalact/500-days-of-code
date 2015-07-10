f4390a7505a42685d44fa453a43c2491a00608a3
Dami Brown <tinaecole@yahoo.com>
2015-07-10 22:16:00 +0100

    javascript function

---
+++ js/tap.js
@@ -0,0 +1,10 @@
+$(function(){
+    var box1click     = $('#box1');
+        
+});
+
+$(function () {
+    $(".box1").click(function () {
+        $(this).css('background-color', '#000000');
+    });
+});
